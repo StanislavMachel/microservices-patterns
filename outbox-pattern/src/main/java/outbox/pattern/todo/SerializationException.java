d3e6aec8f6162e571bfe6e978eebf212c6c2f8a8
Stanislav Matšel <stanislav.machel@gmail.com>
2022-05-17 13:35:54 +0300

    Improve controller and create more complex test

---
+++ outbox-pattern/src/main/java/outbox/pattern/todo/SerializationException.java
@@ -0,0 +1,9 @@
+package outbox.pattern.todo;
+
+public class SerializationException extends RuntimeException {
+
+    public SerializationException(Throwable cause) {
+        super(cause);
+    }
+
+}
